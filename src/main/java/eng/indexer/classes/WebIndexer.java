c8520004102244a3a2b3ec65f2a1b8824e9cda44
John Maurice <johnmaurice71@gmail.com>
2023-05-18 05:06:02 +0300

    fixed word count

--- src/main/java/eng/indexer/classes/WebIndexer.java
+++ src/main/java/eng/indexer/classes/WebIndexer.java
@@ -61,17 +61,19 @@ public class WebIndexer implements Indexer {
         Hashtable<String, ArrayList<WordOccurrence>> wordList = new Hashtable<String, ArrayList<WordOccurrence>>();
         Document document = Jsoup.parse(HTML);
         
-        int wordPosition = 0;
+        ProcessTextReturn ret = new ProcessTextReturn();
         try {
             String keywords = document.select("meta[name=keywords]").get(0).attr("content");
             if (!keywords.trim().isEmpty())
-                wordPosition = processText(keywords, "meta", wordList, wordPosition) + 1;
+                processText(keywords, "meta", wordList, ret);
+                ret.wordPosition += 1;
         } catch (Exception e) { }
 
         try {
             String description = document.select("meta[name=description]").get(0).attr("content");
             if (!description.trim().isEmpty())
-                wordPosition = processText(description, "meta", wordList, wordPosition) + 1;
+                processText(description, "meta", wordList, ret);
+                ret.wordPosition += 1;
         } catch (Exception e) { }
 
         document.getElementsByTag("meta").remove();
@@ -85,43 +87,48 @@ public class WebIndexer implements Indexer {
         document.getElementsByTag("textarea").remove();
         document.getElementsByTag("svg").remove();
 
-        wordPosition = extractWords(document.body(), wordList, wordPosition);
+        extractWords(document.body(), wordList, ret);
 
-        int blockTagcount = 0;
-        for (String tag : BLOCK_TAGS) {
-            blockTagcount += document.select(tag).size();
+        return new ProcessHTMLReturn(wordList, ret.wordCount);
+    }
+
+    private class ProcessTextReturn {
+        public int wordPosition;
+        public int wordCount;
+        public ProcessTextReturn() {
+            this.wordPosition = 0;
+            this.wordCount = 0;
         }
-        return new ProcessHTMLReturn(wordList, wordPosition - blockTagcount);
     }
 
     private static final List<String> BLOCK_TAGS = Arrays.asList("p", "h1", "h2", "h3", "h4", "h5", "h6");
-    private static int processText(String text, String parentType, Hashtable<String, ArrayList<WordOccurrence>> wordList, int wordPosition) {
+    private static void processText(String text, String parentType, Hashtable<String, ArrayList<WordOccurrence>> wordList, ProcessTextReturn ret) {
         String [] sentences = text.split("\\n");
         for (String sentence : sentences) {
             ArrayList<String> words = Preprocessor.preprocess(sentence);
             for (String word : words) {
                 if (wordList.get(word) == null) wordList.put(word, new ArrayList<WordOccurrence>());
-                wordList.get(word).add(new WordOccurrence(wordPosition++, parentType));
+                wordList.get(word).add(new WordOccurrence(ret.wordPosition++, parentType));
+                ret.wordCount++;
             }
-            wordPosition++;
+            ret.wordPosition++;
         }
-        return wordPosition - 1;
+        ret.wordPosition--;
     }
 
-    private static int extractWords(Element element, Hashtable<String, ArrayList<WordOccurrence>> wordList, int wordPosition) {
+    private static void extractWords(Element element, Hashtable<String, ArrayList<WordOccurrence>> wordList, ProcessTextReturn ret) {
         for (var child : element.childNodes()) {
             if (child instanceof Element) {
-                wordPosition = extractWords((Element)child, wordList, wordPosition);
+                extractWords((Element)child, wordList, ret);
             } else if (child instanceof org.jsoup.nodes.TextNode) {
                 String text = ((org.jsoup.nodes.TextNode)child).text();
                 if (text.trim().isEmpty()) continue;
                 String parentType = element.tagName();
-                wordPosition = processText(text, parentType, wordList, wordPosition);
+                processText(text, parentType, wordList, ret);
                 if (BLOCK_TAGS.contains(element.nodeName())) {
-                    wordPosition++;
+                    ret.wordPosition++;
                 }
             }
         }
-        return wordPosition;
     }
 }
